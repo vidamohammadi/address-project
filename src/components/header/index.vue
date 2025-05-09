950ab92207cd66a080528e061f3f1e7639477d5a
vida <m.vidamohammadi@gmail.com>
2025-05-08 23:55:57 -0800

    fix: add links in headers

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,14 +1,21 @@
 <template>
   <v-app-bar color="white" dark>
-    <v-row class="w-100 d-flex align-center px-2 text-body-2" >
-      <v-col cols="4">
-        <v-img src="./logo.png" max-width="50px" max-height="50px"></v-img>
+    <v-row class="w-100 d-flex align-center px-10 text-body-2 justify-lg-space-between " >
+      <v-col cols="4"  >
+        <router-link to="/">
+          <v-img src="./logo.png" max-width="50px" max-height="50px"></v-img>
+
+        </router-link>
       </v-col>
-      <v-col cols="4" >
-        <span class="text-black">ثبت آدرس</span>
-      </v-col>
-      <v-col cols="4" >
-        <router-link to="/address-list" class="text-green">مشاهده آدرس ها</router-link>
+
+      <v-col cols="8" lg="3" class="d-flex justify-end justify-space-between" >
+        <router-link to="/" class="text-decoration-none nav-link" active-class="active-link">
+          ثبت آدرس
+
+        </router-link>
+
+
+        <router-link to="/address-list" class="text-decoration-none nav-link" active-class="active-link" >مشاهده آدرس ها</router-link>
       </v-col>
     </v-row>
   </v-app-bar>
@@ -18,4 +25,12 @@
 </script>
 
 <style scoped>
+.nav-link {
+  color: black;
+  text-decoration: none;
+}
+
+.active-link {
+  color: green;
+}
 </style>
